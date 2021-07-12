dac82127525cad2f0db2e2a16ba93414dc93b776
Kasey <Kastlegate@gmail.com>
2021-07-12 14:40:26 -0400

    Got the remove button working. Finally!

--- books.js
+++ books.js
@@ -3,21 +3,28 @@ let addBookFormButton = document.getElementById("addBookFormButton");
 let cancelButton = document.getElementById("cancelButton");
 let addThisBookButton = document.getElementById("addThisBookButton");
 let cardDisplay = document.getElementById("cardDisplay");
-var newline = "\n";
+const newline = "\n";
+const removeButton = document.getElementById("removeButton");
 
 
 // hides the new book form on initial page load
 document.getElementById("form").style.display = 'none';
 
-// an object contructor to create different books in a Library
-function Book(title, author, pages, read, index)
+// creates an object contructor to create different books in a Library
+function Book(title, author, pages, read)
 {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.index = index;
 }
+
+// creating a couple of default book objects with the Book object constructor
+const book1 = new Book("The Fellowship of The Ring", "J.R.R. Tolkien", "356", "Has been read.");
+const book2 = new Book("A song of Ice and Fire", "George R.R. Martin", "256", "Has not been read.");
+
+// creates an array for the library and populates it with the default book objects
+let myLibrary = [book1, book2];
 
 // function to bring up the form when the add new book button is pressed
 function addBookFormButtonClicked()
@@ -70,6 +77,23 @@ function cancelButtonClicked()
     
 }
 
+function deleteBook(bookToDelete)
+{
+    let index = bookToDelete;
+    if (index > -1) {
+    myLibrary.splice(index, 1);}
+}
+
+// creates a function to remove a particular book when the remove button is pressed
+function removeButtonClicked()
+{
+    console.log(this.getAttribute("data-buttonid"))
+    deleteBook(this.getAttribute("data-buttonid"))
+    document.getElementById("cardDisplay").textContent = "";
+    document.getElementById("display").textContent = addNewCard(myLibrary);
+    console.log(myLibrary);
+}
+
 // creating event listeners for buttons
 addBookFormButton.addEventListener("click", addBookFormButtonClicked);
 addThisBookButton.addEventListener("click", addThisBookButtonClicked);
@@ -77,33 +101,35 @@ cancelButton.addEventListener("click", cancelButtonClicked);
 
 
 
-// creating a couple of default book objects with the Book object constructor
-const book1 = new Book("The Fellowship of The Ring", "J.R.R. Tolkien", "356", "Has been read.");
-const book2 = new Book("A song of Ice and Fire", "George R.R. Martin", "256", "Has not been read.");
-
-// creates an array for the library and populates it with the default book objects
-let myLibrary = [book1, book2];
-
 //function creating a new div element ("card") with the myLibrary array and appending it to display in html
 
 function addNewCard(array)
 {
     array.forEach(element =>
     {
+        // creating a new div in the dom and assigning the card class to it. Then filling out its text with the 
+        // information from the current element in the array.
         let newCard = document.createElement("div");
         newCard.class = "card";
-        newCard.textContent = "Title: " + element.title + newline + "Author: " + element.author + newline + "Page count: " + element.pages + newline + element.read;
+        newCard.dataset.id = array.indexOf(element);
+        newCard.textContent = "Title: " + element.title + newline + "Author: " + 
+        element.author + newline + "Page count: " + element.pages + newline + element.read;
+
+        // adds the new card to the cardDisplay div
         cardDisplay.appendChild(newCard).className = 'card';
+
+        // creates the button for the card and fills in its text
         let removeButton = document.createElement("button");
         removeButton.textContent = "Remove";
-        removeButton.dataset.id = element.id;
+        removeButton.dataset.buttonid = array.indexOf(element);
         newCard.appendChild(removeButton);
+        removeButton.addEventListener("click", removeButtonClicked);
 
-    })
+     })
     
 };
 
-// + <br></br> + "element.author" + <br></br> + <button class="removeButton" data-id="myLibrary(element)" >Remove</button>;
+
 
 // adds the info function to the Book contructor's prototype. This function returns each value in the current book object. 
 // The function being added to the prototype avoids each object (book) creating the functions
@@ -114,9 +140,9 @@ Book.prototype.info = function()
      return this.title + " by " + this.author +  ". " + this.pages + " pages. " + this.read;
 }
 
-function addBookToLibrary(title, author, pages, read, index) 
+function addBookToLibrary(title, author, pages, read) 
 {
-    let newBook = new Book(title, author, pages, read, index);
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 } 
 
@@ -133,4 +159,4 @@ function displayLibrary()
 }
 
 document.getElementById("display").textContent = addNewCard(myLibrary);
-// addBookToLibrary()
+
